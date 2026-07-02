55875225d563d14afd435b64a33f7ce09964d7b0
Laura Bernal <laurabernal1912@gmail.com>
2026-07-02 11:58:33 -0500

    Implementa rotaciones simples del árbol AVL

--- estructuras/avl.py
+++ estructuras/avl.py
@@ -53,3 +53,42 @@ class AVL:
             self._obtener_altura(nodo.izquierda)
             - self._obtener_altura(nodo.derecha)
         )
+        
+        
+        
+    def _rotacion_derecha(self, y):
+        """
+        Realiza una rotación simple hacia la derecha.
+        """
+
+        x = y.izquierda
+        t2 = x.derecha
+
+        # Rotación
+        x.derecha = y
+        y.izquierda = t2
+
+        # Actualizar alturas
+        self._actualizar_altura(y)
+        self._actualizar_altura(x)
+
+        return x
+    
+
+    def _rotacion_izquierda(self, x):
+        """
+        Realiza una rotación simple hacia la izquierda.
+        """
+
+        y = x.derecha
+        t2 = y.izquierda
+
+        # Rotación
+        y.izquierda = x
+        x.derecha = t2
+
+        # Actualizar alturas
+        self._actualizar_altura(x)
+        self._actualizar_altura(y)
+
+        return y
