6455eabe9ec11e060bd4112d8a6d467d519d25cf
Laura Bernal <laurabernal1912@gmail.com>
2026-07-02 11:50:25 -0500

    Agrega NodoAVL y funciones básicas del AVL

--- estructuras/avl.py
+++ estructuras/avl.py
@@ -18,3 +18,38 @@ class AVL:
 
     def __init__(self):
         self.raiz = None
+    
+    
+    def _obtener_altura(self, nodo):
+        """
+        Retorna la altura de un nodo.
+        Si el nodo es None, la altura es 0.
+        """
+        if nodo is None:
+            return 0
+        return nodo.altura
+    
+    
+    def _actualizar_altura(self, nodo):
+        """
+        Actualiza la altura de un nodo según la altura
+        de sus hijos.
+        """
+        nodo.altura = 1 + max(
+            self._obtener_altura(nodo.izquierda),
+            self._obtener_altura(nodo.derecha)
+        )
+        
+        
+        
+    def _factor_balance(self, nodo):
+        """
+        Calcula el factor de balance de un nodo.
+        """
+        if nodo is None:
+            return 0
+
+        return (
+            self._obtener_altura(nodo.izquierda)
+            - self._obtener_altura(nodo.derecha)
+        )
